b2c152b3416654ad63f6c05c0cc3d4cad803edc1
Tanziruz Zaman <143191379+Tanziruz@users.noreply.github.com>
2026-01-15 06:19:19 -0800

    changed vim to nvim

--- pre-chroot.sh
+++ pre-chroot.sh
@@ -22,7 +22,7 @@ PKGS=(
   mesa xf86-video-intel vulkan-intel libva-intel-driver
   bluez bluez-utils
   pipewire pipewire-pulse wireplumber
-  git vim sudo zsh curl go reflector
+  git nvim sudo zsh curl go reflector
 
   # Hyprland (base)
   hyprland wlroots wayland wayland-protocols xorg-xwayland
